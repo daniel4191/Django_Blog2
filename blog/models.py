809ae8e57d4d42406d11dde9cbf64088137631a0
daniel <>
2022-12-31 14:34:43 +0900

    Post models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    # auto_now_add는 "최초 저장시"에만 저장이 되고
+    created_at = models.DateTimeField(auto_now_add=True)
+    # auto_now는 "수정 후 save를 할때마다"최신화 되어 적용된다.
+    updated_at = models.DateTimeField(auto_now=True)
+    # author 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+    'blog',
+    'single_pages'
 ]
 
 MIDDLEWARE = [
@@ -105,11 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
