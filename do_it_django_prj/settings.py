4f15913c4e12930172a6e36d574a0f3cf7e63072
daniel <>
2022-12-31 20:11:29 +0900

    applied bootstrap to post lists pages

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -123,6 +123,9 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+# static 폴더도 프로젝트, 앱단위의 폴더와 동등선상에 놓겠다는 의미
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
