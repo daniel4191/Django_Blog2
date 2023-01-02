76b8db27c245ba8aa2b44472cdb37a36cb518bfe
daniel <>
2023-01-02 20:20:39 +0900

    author field add to models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 import os
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -45,10 +46,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     # auto_now는 "수정 후 save를 할때마다"최신화 되어 적용된다.
     updated_at = models.DateTimeField(auto_now=True)
-    # author 추후 작성 예정
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     # 이것을추가해 줌으로써 admin 페이지에서 볼때는 "View on site"라는 버튼이 생성되었다.
     def get_absolute_url(self):
