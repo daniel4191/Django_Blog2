7432ef3a98b5711e75ba59e151159ede5849fbb0
daniel <>
2023-01-04 21:46:11 +0900

    login functions added google, e-mail

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -43,6 +43,11 @@ INSTALLED_APPS = [
     # installed pip things
     'crispy_forms',
     'markdownx',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 
     # local apps
     'blog',
@@ -144,3 +149,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# 로그인 관련
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/blog/'
