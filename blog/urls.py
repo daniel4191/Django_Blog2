b862ad15a85b9ba50b427d36d05c056ad1d6864f
daniel4191 <daniell8282@naver.com>
2023-01-05 16:54:29 +0900

    added functions about comments delete

--- blog/urls.py
+++ blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/', views.delete_comment),
 ]
