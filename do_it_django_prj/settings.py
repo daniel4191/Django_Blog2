d0ca79ba3ccdec8e074c08b515330d2e6275c806
daniel4191 <daniell8282@naver.com>
2023-02-11 22:47:24 +0900

    girignore added

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -21,10 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hlba+8_n!63j@#luo^pwx+!31te5==!3t=&ntk^j&3y7u-5#l3'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-hlba+8_n!63j@#luo^pwx+!31te5==!3t=&ntk^j&3y7u-5#l3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
+
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
+
 
 ALLOWED_HOSTS = []
 
